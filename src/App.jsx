39ef8e783a2842d001b5a9896590f72033dab605
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2024-10-18 08:38:45 +0100

    PrimerAnimal

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { CustomButton } from "milibreria";
+
+function App() {
+  const datos={
+    animal: 'animal',
+    imagenUrl: '/perro.jpg',
+    imagenSize: 150,
+  }
+  return (
+    <>
+      <h1>Soy un {datos.animal} y vivo feliz</h1>
+      <img className='avatar' src={datos.imagenUrl} alt='img' style={{width: datos.imagenSize, height: datos.imagenSize}}/>
+      <CustomButton
+                text="Buenas"
+                textColor="white"
+                backgroundColor="red"
+                borderColor="green"
+                size="medium"
+                disabled={!acceptedTerms}
+              />  
+    </>
+  )
+}
+
+export default App
