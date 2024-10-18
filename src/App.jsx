f2f3b6414e9a6b3a445e7753232797b8fb07e9f8
Palomo2001 <pablovegaalamo@alumno.ieselrincon.es>
2024-10-18 08:59:52 +0100

    TercerAnimal

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 import { CustomButton } from "milibreria";
 
 function App() {
-  const datos={
+  const [count, setCount] = useState(0); 
+
+  const datos = {
     animal: 'animal',
     imagenUrl: '/perro.jpg',
     imagenSize: 150,
+  };
+
+  function handleClick() {
+    setCount(count + 1); 
   }
+
   return (
     <>
       <h1>Soy un {datos.animal} y vivo feliz</h1>
-      <img className='avatar' src={datos.imagenUrl} alt='img' style={{width: datos.imagenSize, height: datos.imagenSize}}/>
-        
+      <img
+        className='avatar'
+        src={datos.imagenUrl}
+        alt='img'
+        style={{ width: datos.imagenSize, height: datos.imagenSize }}
+      />
+      
+      <CustomButton
+        text={`Me picastes ${count} veces`}                        
+        size="medium"                      
+        onClick={handleClick}              
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
